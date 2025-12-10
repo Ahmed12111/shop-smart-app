2de0f1856b4e28a7925778942a815dd6badfda84
Ahmed Nour <an1977304@gmail.com>
2025-12-10 19:52:58 +0200

    Fix: Fetch orders by current user only

--- lib/providers/order_provider.dart
+++ lib/providers/order_provider.dart
@@ -13,27 +13,29 @@ class OrdersProvider with ChangeNotifier {
     User? user = auth.currentUser;
     var uid = user!.uid;
     try {
-      await FirebaseFirestore.instance.collection("ordersAdvanced").get().then((
-        orderSnapshot,
-      ) {
-        orders.clear();
-        for (var element in orderSnapshot.docs) {
-          orders.insert(
-            0,
-            OrdersModel(
-              orderId: element.get('orderId'),
-              productId: element.get('productId'),
-              userId: element.get('userId'),
-              price: element.get('price').toString(),
-              productTitle: element.get('productTitle').toString(),
-              quantity: element.get('quantity').toString(),
-              imageUrl: element.get('imageUrl'),
-              userName: element.get('userName'),
-              orderDate: element.get('orderDate'),
-            ),
-          );
-        }
-      });
+      await FirebaseFirestore.instance
+          .collection("ordersAdvanced")
+          .where("userId", isEqualTo: uid)
+          .get()
+          .then((orderSnapshot) {
+            orders.clear();
+            for (var element in orderSnapshot.docs) {
+              orders.insert(
+                0,
+                OrdersModel(
+                  orderId: element.get('orderId'),
+                  productId: element.get('productId'),
+                  userId: element.get('userId'),
+                  price: element.get('price').toString(),
+                  productTitle: element.get('productTitle').toString(),
+                  quantity: element.get('quantity').toString(),
+                  imageUrl: element.get('imageUrl'),
+                  userName: element.get('userName'),
+                  orderDate: element.get('orderDate'),
+                ),
+              );
+            }
+          });
       return orders;
     } catch (e) {
       rethrow;
